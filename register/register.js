bb243fcbf79dd853f3c4925f95fa9ad9326385f4
shahHasib <codewithhassu@gmail.com>
2024-08-24 21:18:57 +0530

    init

--- register/register.js
+++ register/register.js
@@ -1,19 +1,19 @@
 document.addEventListener('DOMContentLoaded', function() {
     const categorySelect = document.getElementById('category');
-    const user = document.getElementById('username');
-    const emailContainer = document.getElementById('email');
-    const phoneContainer = document.getElementById('phone');
-
+    const user = document.getElementById('user');
+    const emailContainer = document.getElementById('em');
+    const phoneContainer = document.getElementById('ph');
+    phoneContainer.style.display = 'none';
     categorySelect.addEventListener('change', function() {
         const selectedCategory = this.value;
 
         if (selectedCategory === 'guest') {
             user.style.display = 'none';
             emailContainer.style.display = 'none';
-            phoneContainer.style.display = 'block';
+            phoneContainer.style.display = 'flex';
         } else {
-            user.style.display = 'block';
-            emailContainer.style.display = 'block';
+            user.style.display = 'flex';
+            emailContainer.style.display = 'flex';
             phoneContainer.style.display = 'none';
         }
     });
